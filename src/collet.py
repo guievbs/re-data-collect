f90f5d46c01721ac4a0b5358933464c35f4bd7ef
Guilherme Sebastião <guivbsebas@gmail.com>
2024-05-01 19:12:06 -0300

    feat(core): Add utility functions for data processing
    - Implemented new functions to improve code modularity and maintainability.
    - Added functions for data validation, parsing, and transformation.
    - Refactored existing code to utilize the new utility functions, enhancing readability and reducing duplication.

--- src/collet.py
+++ src/collet.py
@@ -16,34 +16,60 @@ def get_content(url, headers_json='../json/headers.json') -> requests.Response:
 
 def transform_info(ems) -> dict:
     info = {}
+    # Iterate over each emphasized text
     for em in ems:
-        key, value = em.text.split(":")
+        key, value, *_ = em.text.split(":")
         key = key.strip(" ")
         info[key] = value.strip(" ")
     
     return info
 
-def get_basic_infos(soup) -> dict:    
-    # Find the div element containing the main content
-    div_content = soup.find('div', class_='td-page-content')
-    paragraph = div_content.find_all('p')[1]
+def get_basic_infos(content) -> dict:   
+    paragraph = content.find_all('p')[1]
     ems = paragraph.find_all('em')
-
     # Function to transform <em> HTML tags into a Python dict
     data = transform_info(ems)
-
     return data 
 
-url = 'https://www.residentevildatabase.com/personagens/ada-wong/'
+def get_apparitions(content) -> list:
+        
+    li_elements = content.find('h4').find_next().find_all('li')
 
-response = get_content(url)
+    apparitions:list = [li.text for li in li_elements]
+    return apparitions
 
-status:int = response.status_code
+def get_characters_infos(url) -> dict:
+    response = get_content(url)
+    status = response.status_code
 
-if status != 200:
-    print(f"something went wrong: code {status}")
+    if status != 200:
+        print(f"Something went wrong: code {status}")
+        return {}  # Return empty dictionary if there's an error
+    else:
+        soup = BeautifulSoup(response.text, features="lxml")
+        content = soup.find("div", class_='td-page-content')
+        data = get_basic_infos(content)
+        data['Apparitions'] = get_apparitions(content)
+        return data
 
-# Initialize BeautifulSoup with the HTML response text
-soup = BeautifulSoup(response.text, features="lxml")
 
-get_basic_infos(soup)
+def get_links() -> list: 
+    url = 'https://www.residentevildatabase.com/personagens/'
+    response = get_content(url)
+    characters_text = BeautifulSoup(response.text, features="lxml")
+    links_html = characters_text.find("div", class_='td-page-content').find_all('a')
+    links:list = [link['href'] for link in links_html]  
+    
+    return links
+
+links_characters:list = get_links()
+data_characters = []
+
+for link in links_characters:
+    print(link)
+    character = get_characters_infos(link)
+    if character != None:  # Check if the character dictionary is not empty
+        character['Link'] = link  # Assign the link to the character dictionary
+        data_characters.append(character)
+    else:
+        print(f"Failed to retrieve data for {link}. Skipping...")
