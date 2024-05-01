650106ae75346c968e1ae47c943e5eab10acfd43
Guilherme Sebastião <guivbsebas@gmail.com>
2024-05-01 17:28:05 -0300

    refactor: add functions to better readability

--- src/collet.py
+++ src/collet.py
@@ -1,27 +1,49 @@
 import requests
+import json
 from bs4 import BeautifulSoup
+
+def get_headers(file_path) -> any:
+    with open(file_path, 'r') as file:
+        headers = json.load(file)
+    
+    return headers
+
+def get_content(url, headers_json='../json/headers.json') -> requests.Response:
+    headers = get_headers(headers_json)
+    response = requests.get(url, headers=headers)
+
+    return response
+
+def transform_info(ems) -> dict:
+    info = {}
+    for em in ems:
+        key, value = em.text.split(":")
+        key = key.strip(" ")
+        info[key] = value.strip(" ")
+    
+    return info
+
+def get_basic_infos(soup) -> dict:    
+    # Find the div element containing the main content
+    div_content = soup.find('div', class_='td-page-content')
+    paragraph = div_content.find_all('p')[1]
+    ems = paragraph.find_all('em')
+
+    # Function to transform <em> HTML tags into a Python dict
+    data = transform_info(ems)
+
+    return data 
 
 url = 'https://www.residentevildatabase.com/personagens/ada-wong/'
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    # 'Accept-Encoding': 'gzip, deflate, br',
-    'Referer': 'https://www.residentevildatabase.com/personagens/',
-    'DNT': '1',
-    'Sec-GPC': '1',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-User': '?1',
-}
+response = get_content(url)
 
-response = requests.get(url, headers=headers)
-
-status = response.status_code
+status:int = response.status_code
 
 if status != 200:
     print(f"something went wrong: code {status}")
+
+# Initialize BeautifulSoup with the HTML response text
+soup = BeautifulSoup(response.text, features="lxml")
+
+get_basic_infos(soup)
